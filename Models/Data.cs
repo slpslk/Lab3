955a39eb6eddcc293c623070b8e1bdd6d168663e
Ivan Kovalev <105602023+Chmilko@users.noreply.github.com>
2023-06-04 21:32:21 +0700

    id added

--- Models/Data.cs
+++ Models/Data.cs
@@ -4,6 +4,7 @@ namespace Lab1_3.Models
 {
     public class PlayerData
     {
+        public Guid Id { get; set; } = Guid.Empty;
         public string Nickname { get; set; }
         public int Level { get; set; }
         public string Clan { get; set; }
