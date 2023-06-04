90e7803c5587dc6045d2ebc522e3acbf3b4b64cd
Ivan Kovalev <105602023+Chmilko@users.noreply.github.com>
2023-06-04 21:27:33 +0700

    new version controller added

---
+++ Models/Version.cs
@@ -0,0 +1,9 @@
+namespace Lab1_3.Models
+{
+    public class Version
+    {
+        public string Company { get; set; }
+        public string ProductVersion { get; set; }
+        public string Product { get; set; }
+    }
+}
