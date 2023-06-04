bd2d5a4d48feeeb2ba6db10d6c8450d2af11d315
Ivan Kovalev <105602023+Chmilko@users.noreply.github.com>
2023-06-04 21:53:52 +0700

    istorage added and update controller

--- Controllers/Controller.cs
+++ Controllers/Controller.cs
@@ -2,49 +2,66 @@ using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Lab1_3.Models;
+using Lab1_3.Storage;
 
-namespace Lab1_3.Controllers
+namespace pavlovLab.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    public class Lab1Controller : ControllerBase
+    public class LabController : ControllerBase
     {
-        private static List<PlayerData> _memCache = new List<PlayerData>();
+        private static IStorage<PlayerData> _memCache = new MemCache();
 
         [HttpGet]
         public ActionResult<IEnumerable<PlayerData>> Get()
         {
-            return _memCache;
+            return Ok(_memCache.All);
         }
 
         [HttpGet("{id}")]
-        public ActionResult<PlayerData> Get(int id)
+        public ActionResult<PlayerData> Get(Guid id)
         {
-            if (_memCache.Count <= id) throw new IndexOutOfRangeException("Такого игрока нет");
+            if (!_memCache.Has(id)) return NotFound("No such");
 
-            return _memCache[id];
+            return Ok(_memCache[id]);
         }
 
         [HttpPost]
-        public void Post([FromBody] PlayerData value)
+        public IActionResult Post([FromBody] PlayerData value)
         {
+            var validationResult = value.Validate();
+
+            if (!validationResult.IsValid) return BadRequest(validationResult.Errors);
+
             _memCache.Add(value);
+
+            return Ok($"{value.ToString()} has been added");
         }
 
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] PlayerData value)
+        public IActionResult Put(Guid id, [FromBody] PlayerData value)
         {
-            if (_memCache.Count <= id) throw new IndexOutOfRangeException("Такого игрока нет");
+            if (!_memCache.Has(id)) return NotFound("No such");
 
+            var validationResult = value.Validate();
+
+            if (!validationResult.IsValid) return BadRequest(validationResult.Errors);
+
+            var previousValue = _memCache[id];
             _memCache[id] = value;
+
+            return Ok($"{previousValue.ToString()} has been updated to {value.ToString()}");
         }
 
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(Guid id)
         {
-            if (_memCache.Count <= id) throw new IndexOutOfRangeException("Такого игрока нет");
+            if (!_memCache.Has(id)) return NotFound("No such");
 
+            var valueToRemove = _memCache[id];
             _memCache.RemoveAt(id);
+
+            return Ok($"{valueToRemove.ToString()} has been removed");
         }
     }
 }
--- Models/Data.cs
+++ Models/Data.cs
@@ -9,5 +9,17 @@ namespace Lab1_3.Models
         public int Level { get; set; }
         public string Clan { get; set; }
         public string Country { get; set; }
+
+        public BaseModelValidationResult Validate()
+        {
+            var validationResult = new BaseModelValidationResult();
+
+            if (string.IsNullOrWhiteSpace(Nickname)) validationResult.Append($"Nickname cannot be empty");
+            if (Level < 0 || Level > 500) validationResult.Append($"Invalid level. Level {Level} is out of range (0..500)");
+            if (string.IsNullOrWhiteSpace(Clan)) validationResult.Append($"Clan cannot be empty");
+            if (string.IsNullOrWhiteSpace(Country)) validationResult.Append($"Coutry cannot be empty");
+            
+            return validationResult;
+        }
     }
 }
